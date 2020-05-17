6df08bf568456d7c9e41915a76e4bbb09ecdee1f
Joshua-Bela <52637337+Joshua-Bela@users.noreply.github.com>
2020-05-16 22:44:23 -0500

    Add files via upload

--- R Script for Data Engineering.R
+++ R Script for Data Engineering.R
@@ -1,12 +1,16 @@
+# Set directories for input and output file
 input_directory = 'C:/Users/jgbel/Desktop/data/us-counties.csv'
 output_directory = 'C:/Users/jgbel/Desktop/data/us-counties0.csv'
 ###########################################################################################
+# Import data
 data = read.csv(input_directory)
+# Import libraries
 library(magrittr)
 library(aTSA)
 library(plyr)
 library(forecast)
 
+# Create new data frame, summarized as total cases and deaths by day
 data0 = ddply(
   data,
   .(date),
@@ -15,12 +19,16 @@ data0 = ddply(
   deaths_sum = sum(deaths)
 )
 
+# Create data frame where each row has the three ARIMA parameters
+# and the MSE (mean squared error) for the corresponding ARIMA model.
+# The first row is just 0's.  It will be deleted later.
 results = data.frame(
   p = 0,
   d = 0,
   q = 0,
   MSE = 0
 )
+# Perform a grid search for the three parameters values to find the optimal model.
 for(p in 0:4){
   for(d in 0:2){
     for(q in 0:4){
@@ -37,9 +45,13 @@ for(p in 0:4){
     }
   }
 }
+# Delete the first row of the grid search results.
 results = results[-1,]
+# Sort the rows so that the best models (models with the lowest MSE) are at the top.
 results = results[order(results$MSE),]
+# Print out information for the top 7 models.
 results[1:7,]
 
 ###########################################################################################
+# Write the new, engineered data frame to a CSV in the output directory.
 write.csv(data0, output_directory, row.names = F)
